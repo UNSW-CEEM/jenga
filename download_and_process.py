5a9ac6894fa292f7b36e2c940300b0084b544e78
Luke Marshall <lukasmarshall@gmail.com>
2019-01-23 15:14:55 +0900

    A little more NEMOSYS integration and automation

--- download_and_process.py
+++ download_and_process.py
@@ -13,30 +13,35 @@ def check_file_exists(path, fname):
 
 
 
+def run_period_offers(year, month):
+    expected_fname = "PUBLIC_DVD_BIDPEROFFER_D_"+str(year)+str(month).zfill(2)+"010000.CSV"
+    # If the file isn't in the folder, use nemosis to download it. 
+    if not check_file_exists(os.path.join(raw_data_cache, 'BIDPEROFFER_D'), expected_fname):
+        print("File not found - fetching via NEMOSIS", expected_fname)
+        start_time = pendulum.datetime(year,month,1,0,0).format('YYYY/MM/DD HH:mm:ss')
+        end_time = pendulum.datetime(year,month,1,0,5).format('YYYY/MM/DD HH:mm:ss')
+        print(start_time, end_time)
+        # end_time = '2018/01/01 00:05:00'
+        table = 'BIDPEROFFER_D'
+        data_fetch_methods.dynamic_data_compiler(start_time, end_time, table, os.path.join(raw_data_cache, 'bid_period_offers'))
+    # Load the file into the database
+    process_period_offers.process(os.path.join(raw_data_cache,'BIDPEROFFER_D',expected_fname))
 
 
 
 
-raw_data_cache = 'data'
 
-years = [2018]
-months = [7]
+if __name__=="__main__":
 
-for year in years:
-    for month in months:
-        # Check if expected bid period offer is there
-        expected_fname = "PUBLIC_DVD_BIDPEROFFER_D_"+str(year)+str(month).zfill(2)+"010000.CSV"
-        # If the file isn't in the folder, use nemosis to download it. 
-        if not check_file_exists(os.path.join(raw_data_cache, 'BIDPEROFFER_D'), expected_fname):
-            print("File not found - fetching via NEMOSIS", expected_fname)
-            start_time = pendulum.datetime(year,month,1,0,0).format('YYYY/MM/DD HH:mm:ss')
-            end_time = pendulum.datetime(year,month,1,0,5).format('YYYY/MM/DD HH:mm:ss')
-            print(start_time, end_time)
-            # end_time = '2018/01/01 00:05:00'
-            table = 'BIDPEROFFER_D'
-            data_fetch_methods.dynamic_data_compiler(start_time, end_time, table, os.path.join(raw_data_cache, 'bid_period_offers'))
-        # Load the file into the database
-        process_period_offers.process(os.path.join(raw_data_cache,'BIDPEROFFER_D',expected_fname))
+    raw_data_cache = 'data'
+
+    years = [2018]
+    months = [6]
+
+    for year in years:
+        for month in months:
+        run_period_offers(year, month)
+        run_day_offers(year, month)
         
        
 
